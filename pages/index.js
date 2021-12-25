eb9ca2e0b981663440e43b5d23a83999049bee13
Divyansh <divyanshvermafast4@gmail.com>
2021-12-25 14:18:00 +0530

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
       <Head>
-        <title>Instagram</title>
+        <title>instagram-div</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
