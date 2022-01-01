7796f9f17aa06536e61ff0887abc1250b98a1752
Divyansh <divyanshvermafast4@gmail.com>
2022-01-01 21:10:28 +0530

    group initil

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,10 +7,10 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      secret: process.env.SECRET,
     }),
     // ...add more providers here
   ],
+  secret: process.env.SECRET,
 
   // Default log-in screen
   // theme: {
